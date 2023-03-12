313d9e8ad3fa3ef18433dbec42d641ec460f952f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-03-13 00:11:12 +0900

    Update check-all-modules.sh

--- modprobe/src/check-all-modules.sh
+++ modprobe/src/check-all-modules.sh
@@ -48,7 +48,7 @@ function listpci() {
             class="$(echo $line | cut -c 9-12)"
             vendor="$(echo $line | cut -c 15-18)"
             device="$(echo $line | cut -c 20-23)"        
-            ;;
+        ;;
         r1000 )
             case $MODEL in
             923+ )
@@ -64,15 +64,30 @@ function listpci() {
                 device="$(echo $line | cut -c 25-28)"
                 ;;
             esac
-            ;;
-        geminilake | bromolow | apollolake | broadwell | broadwellnk | *)
+        ;;
+        broadwellnk )
+            case $MODEL in
+            sa3600 )
+                bus="$(echo $line | cut -c 1-7)"
+                class="$(echo $line | cut -c 9-12)"
+                vendor="$(echo $line | cut -c 15-18)"
+                device="$(echo $line | cut -c 20-23)"
+                ;;
+            *)
+                bus="$(echo $line | cut -c 6-12)"
+                class="$(echo $line | cut -c 14-17)"
+                vendor="$(echo $line | cut -c 20-23)"
+                device="$(echo $line | cut -c 25-28)"
+                ;;
+            esac
+        ;;
+        geminilake | bromolow | apollolake | broadwell | *)
             bus="$(echo $line | cut -c 6-12)"
             class="$(echo $line | cut -c 14-17)"
             vendor="$(echo $line | cut -c 20-23)"
             device="$(echo $line | cut -c 25-28)"
-            ;;
+        ;;
         esac
-
 
         #echo "PCI : $bus Class : $class Vendor: $vendor Device: $device"
         case $class in
