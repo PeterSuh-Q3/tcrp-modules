5d79a8e53bf0f0b54f8e695634d45f0a60eb5889
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-02-27 13:02:27 +0900

    Update syncsha.sh

--- all-modules/syncsha.sh
+++ all-modules/syncsha.sh
@@ -1,155 +1,62 @@
 #!/bin/bash
 
+# SHA256 체크섬 계산
 installsha=$(sha256sum ./src/install.sh | awk '{print $1}')
-echo "$installsha"
-
 sbintgzsha=$(sha256sum ./releases/sbin.tgz | awk '{print $1}')
-echo "$sbintgzsha"
+firmwaresha=$(curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/files-chksum && grep firmware.tgz files-chksum | grep firmware.sha256|awk '{print $1}')
 
-cd ./releases/
+# firmware 다운로드
+curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/firmware.tgz
 
-URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/files-chksum"
-echo "$URL"
-curl -kLO $URL
-
-firmware=`grep firmware.tgz files-chksum | grep firmware.sha256|awk '{print $1}'`
-echo "firmware sha256=$firmware"
-
-URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/firmware.tgz"
-echo "$URL"
-curl -kLO $URL
-
-#don't touch bromolow,braswell 2024.12.22
-#Add bromolow again 2025.02.17
+# 플랫폼별 처리
 for platform in bromolow epyc7002; do
-    echo "modify $platform.json"
-    
     if [ "$platform" = "bromolow" ]; then
         kver="3.10.108"
-
-        value=`grep ${platform}-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-        echo "$value"    
-    
-        org=$(jq -r '.files[0].sha256' "${platform}.json")
-        sed -i "s/$org/$value/" "${platform}.json"
-    
-        orgfirmware=$(jq -r '.files[1].sha256' "${platform}.json")
-        sed -i "s/$orgfirmware/$firmware/" "${platform}.json"
-    
-        orginstall=$(jq -r '.files[2].sha256' "${platform}.json")
-        sed -i "s/$orginstall/$installsha/" "${platform}.json"
-    
-        orgsbintgz=$(jq -r '.files[3].sha256' "${platform}.json")
-        sed -i "s/$orgsbintgz/$sbintgzsha/" "${platform}.json"
-
-        URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-${kver}.tgz"
-        echo "$URL"
-        curl -kLO $URL
-        
+        value=$(grep ${platform}-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+        jsonfile="${platform}.json"
     elif [ "$platform" = "epyc7002" ]; then
         kver="5.10.55"
-
-        # 7.1 remark to use rr's module
-        value=`grep ${platform}-7.1-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-        echo "$value"    
-    
-        org=$(jq -r '.files[0].sha256' "${platform}.json")
-        sed -i "s/$org/$value/" "${platform}.json"
-    
-        orgfirmware=$(jq -r '.files[1].sha256' "${platform}.json")
-        sed -i "s/$orgfirmware/$firmware/" "${platform}.json"
-    
-        orginstall=$(jq -r '.files[2].sha256' "${platform}.json")
-        sed -i "s/$orginstall/$installsha/" "${platform}.json"
-    
-        orgsbintgz=$(jq -r '.files[3].sha256' "${platform}.json")
-        sed -i "s/$orgsbintgz/$sbintgzsha/" "${platform}.json"
-
-        URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-7.1-${kver}.tgz"
-        echo "$URL"
-        curl -kLO $URL
-
-        # 7.2 remark to use rr's module
-        value=`grep ${platform}-7.2-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-        echo "$value"    
-    
-        org=$(jq -r '.files[0].sha256' "${platform}72.json")
-        sed -i "s/$org/$value/" "${platform}72.json"
-    
-        orgfirmware=$(jq -r '.files[1].sha256' "${platform}72.json")
-        sed -i "s/$orgfirmware/$firmware/" "${platform}72.json"
-    
-        orginstall=$(jq -r '.files[2].sha256' "${platform}72.json")
-        sed -i "s/$orginstall/$installsha/" "${platform}72.json"
-    
-        orgsbintgz=$(jq -r '.files[3].sha256' "${platform}72.json")
-        sed -i "s/$orgsbintgz/$sbintgzsha/" "${platform}72.json"
-
-        URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-7.2-${kver}.tgz"
-        echo "$URL"
-        curl -kLO $URL
+        value=$(grep ${platform}-7.1-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+        jsonfile="${platform}.json"
         
+        # epyc7002의 7.2 버전 처리
+        value72=$(grep ${platform}-7.2-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+        jsonfile72="${platform}72.json"
     fi
     
+    # bromolow 및 epyc7002 처리
+    if [ "$platform" = "bromolow" ]; then
+        sed -i "s/$(jq -r '.files[0].sha256' "${jsonfile}")/$value/; s/$(jq -r '.files[1].sha256' "${jsonfile}")/$firmwaresha/; s/$(jq -r '.files[2].sha256' "${jsonfile}")/$installsha/; s/$(jq -r '.files[3].sha256' "${jsonfile}")/$sbintgzsha/" "${jsonfile}"
+        curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-${kver}.tgz
+    elif [ "$platform" = "epyc7002" ]; then
+        sed -i "s/$(jq -r '.files[0].sha256' "${jsonfile}")/$value/; s/$(jq -r '.files[1].sha256' "${jsonfile}")/$firmwaresha/; s/$(jq -r '.files[2].sha256' "${jsonfile}")/$installsha/; s/$(jq -r '.files[3].sha256' "${jsonfile}")/$sbintgzsha/" "${jsonfile}"
+        curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-7.1-${kver}.tgz
+        
+        # epyc7002의 7.2 버전 처리
+        sed -i "s/$(jq -r '.files[0].sha256' "${jsonfile72}")/$value72/; s/$(jq -r '.files[1].sha256' "${jsonfile72}")/$firmwaresha/; s/$(jq -r '.files[2].sha256' "${jsonfile72}")/$installsha/; s/$(jq -r '.files[3].sha256' "${jsonfile72}")/$sbintgzsha/" "${jsonfile72}"
+        curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-7.2-${kver}.tgz
+    fi
 done
 
-
+# 다른 플랫폼 처리
 for platform in apollolake broadwell broadwellnk denverton geminilake v1000 r1000 broadwellnkv2 broadwellntbap purley; do
-    echo "modify $platform.json"
-    
     kver="4.4.180"
+    value=$(grep ${platform}-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+    jsonfile="${platform}.json"
     
-    value=`grep ${platform}-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-    echo "$value"    
-
-    org=$(jq -r '.files[0].sha256' "${platform}.json")
-    sed -i "s/$org/$value/" "${platform}.json"
-
-    value=`grep ${platform}-4.4.302.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-    echo "$value"
+    # 4.4.302 버전 처리
+    value302=$(grep ${platform}-4.4.302.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+    jsonfile302="${platform}.json"
     
-    org=$(jq -r '.files[1].sha256' "${platform}.json")
-    sed -i "s/$org/$value/" "${platform}.json"
-
-    orgfirmware=$(jq -r '.files[2].sha256' "${platform}.json")
-    sed -i "s/$orgfirmware/$firmware/" "${platform}.json"
-
-    orginstall=$(jq -r '.files[3].sha256' "${platform}.json")
-    sed -i "s/$orginstall/$installsha/" "${platform}.json"
-
-    orgsbintgz=$(jq -r '.files[4].sha256' "${platform}.json")
-    sed -i "s/$orgsbintgz/$sbintgzsha/" "${platform}.json"
-
-    URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-${kver}.tgz"
-    echo "$URL"
-    curl -kLO $URL
+    # 72 버전 처리
+    value72=$(grep ${platform}-4.4.302.tgz files-chksum | grep modpack.sha256|awk '{print $1}')
+    jsonfile72="${platform}72.json"
     
+    # 처리
+    sed -i "s/$(jq -r '.files[0].sha256' "${jsonfile}")/$value/; s/$(jq -r '.files[1].sha256' "${jsonfile}")/$value302/; s/$(jq -r '.files[2].sha256' "${jsonfile}")/$firmwaresha/; s/$(jq -r '.files[3].sha256' "${jsonfile}")/$installsha/; s/$(jq -r '.files[4].sha256' "${jsonfile}")/$sbintgzsha/" "${jsonfile}"
+    curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-${kver}.tgz
+    
+    # 72 버전 처리
+    sed -i "s/$(jq -r '.files[0].sha256' "${jsonfile72}")/$value72/; s/$(jq -r '.files[1].sha256' "${jsonfile72}")/$firmwaresha/; s/$(jq -r '.files[2].sha256' "${jsonfile72}")/$installsha/; s/$(jq -r '.files[3].sha256' "${jsonfile72}")/$sbintgzsha/" "${jsonfile72}"
+    curl -kLO https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-4.4.302.tgz
 done
-
-for platform in apollolake broadwell broadwellnk denverton geminilake v1000 r1000 broadwellnkv2 broadwellntbap purley; do
-    echo "modify ${platform}72.json"
-
-    kver="4.4.302"
-    
-    value=`grep ${platform}-${kver}.tgz files-chksum | grep modpack.sha256|awk '{print $1}'`
-    echo "$value"
-    
-    org=$(jq -r '.files[0].sha256' "${platform}72.json")
-    sed -i "s/$org/$value/" "${platform}72.json"
-
-    orgfirmware=$(jq -r '.files[1].sha256' "${platform}72.json")
-    sed -i "s/$orgfirmware/$firmware/" "${platform}72.json"
-
-    orginstall=$(jq -r '.files[2].sha256' "${platform}72.json")
-    sed -i "s/$orginstall/$installsha/" "${platform}72.json"
-
-    orgsbintgz=$(jq -r '.files[3].sha256' "${platform}72.json")
-    sed -i "s/$orgsbintgz/$sbintgzsha/" "${platform}72.json"
-
-    URL="https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${platform}-${kver}.tgz"
-    echo "$URL"
-    curl -kLO $URL
-    
-done
-
-#git add .; git commit -am "lastest releases"; git push;
