b63c2065ac6518afc89e282a38eb239e6392144c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-28 14:18:56 +0900

    Create syncinstall.sh

---
+++ all-modules/syncinstall.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+installsha=$(sha256sum ./src/install.sh | awk '{print $1}')
+echo "$installsha"
+
+cd ./releases/
+
+#don't touch bromolow,braswell 2024.12.22
+#Add bromolow again 2025.02.17
+for platform in bromolow avoton braswell cedarview grantley epyc7002 v1000nk; do
+    echo "modify $platform.json"
+    
+    if [ "$platform" = "epyc7002" ]||[ "$platform" = "v1000nk" ]; then
+        kver="5.10.55"
+
+        # 7.1 remark to use rr's module
+        orginstall=$(jq -r '.files[2].sha256' "${platform}.json")
+        sed -i "s/$orginstall/$installsha/" "${platform}.json"
+
+        # 7.2 remark to use rr's module
+        orginstall=$(jq -r '.files[2].sha256' "${platform}72.json")
+        sed -i "s/$orginstall/$installsha/" "${platform}72.json"
+    else
+        kver="3.10.108"
+   
+        orginstall=$(jq -r '.files[2].sha256' "${platform}.json")
+        sed -i "s/$orginstall/$installsha/" "${platform}.json"
+    fi
+    
+done
+
+for platform in apollolake broadwell broadwellnk denverton geminilake v1000 r1000 broadwellnkv2 broadwellntbap purley; do
+    echo "modify $platform.json"
+    
+    kver="4.4.180"
+
+    orginstall=$(jq -r '.files[3].sha256' "${platform}.json")
+    sed -i "s/$orginstall/$installsha/" "${platform}.json"
+
+    orginstall=$(jq -r '.files[2].sha256' "${platform}72.json")
+    sed -i "s/$orginstall/$installsha/" "${platform}72.json"
+    
+done
+
+#git add .; git commit -am "lastest releases"; git push;
