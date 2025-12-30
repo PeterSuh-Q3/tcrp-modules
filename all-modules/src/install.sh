2ba248e415d79b763c732e99725d4097e7b99358
PeterSuh-Q3 <dante9000@gmail.com>
2025-12-30 18:54:57 +0900

    update install.sh

--- all-modules/src/install.sh
+++ all-modules/src/install.sh
@@ -45,7 +45,7 @@ elif [ "${1}" = "late" ]; then
   #fi
   /usr/sbin/depmod -a
   if [ "$TARGET_PLATFORM" = "broadwell" ]||[ "$TARGET_PLATFORM" = "broadwellnk" ]; then
-    ls -l /lib/modules/dca.ko
+    #ls -l /lib/modules/dca.ko
     [ -f /lib/modules/dca.ko ] && modprobe dca && echo "dca loaded"
   fi
 fi
