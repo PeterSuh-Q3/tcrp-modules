96b85a4475ab8fb8a5dd2742a58576118f37310a
PeterSuh-Q3 <dante9000@gmail.com>
2026-03-11 22:46:55 +0900

    install modules oin_load

--- all-modules/src/install.sh
+++ all-modules/src/install.sh
@@ -35,7 +35,7 @@ if [ "${1}" = "modules" ]; then
 elif [ "${1}" = "late" ]; then
   echo "all-modules - ${1}"
 
-  gunzip -c /exts/all-modules/*${TARGET_PLATFORM}*${LINUX_VER}.tgz | tar xvf - -C /tmpRoow/lib/modules/ >/dev/null 2>&1
+  gunzip -c /exts/all-modules/*${TARGET_PLATFORM}*${LINUX_VER}.tgz | tar xvf - -C /tmpRoot/lib/modules/ >/dev/null 2>&1
 
   #if lsmod | grep -q "^r8168_tx"; then
   #  rm /tmpRoot/lib/modules/r8168.ko && echo "tmpRoot r8168.ko removed" || echo "Failed to remove tmpRoot r8168.ko"
